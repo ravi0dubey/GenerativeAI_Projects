50c9f917072842482bb34030d0288409decc3cb2
ravi0dubey <ravi0dubey@gmail.com>
2025-02-09 19:02:53 -0500

    adding project chatbot_using_langchain

---
+++ 1_chatbot_using_Langchain/chatbot/app.py
@@ -0,0 +1,9 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+print("hello")
