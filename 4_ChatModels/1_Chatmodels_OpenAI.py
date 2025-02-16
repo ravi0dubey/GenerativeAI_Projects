f7636ca7d112e7b8ef66a7e08829f196ee9203f5
ravi0dubey <ravi0dubey@gmail.com>
2025-02-16 16:47:11 -0500

    Adding chatmodels_OpenAI.py

--- 4_ChatModels/1_Chatmodels_OpenAI.py
+++ 4_ChatModels/1_Chatmodels_OpenAI.py
@@ -1,8 +1,13 @@
 from langchain_openai import ChatOpenAI
 import os
+chat_model_openapi= ChatOpenAI(model = "gpt-4",temperature=0, api_key=os.getenv("OPEN_API_KEY") )
+result_opeanapi = chat_model_openapi.invoke("Who is President of USA")
+print(result_opeanapi.content)
 
-# chat_model_openapi= ChatOpenAI(model = "chatgpt-4o-latest",temperature=0, api_key=os.getenv("OPEN_API_KEY") )
+chat_model_openapi= ChatOpenAI(model = "chatgpt-4o-latest",temperature=0, api_key=os.getenv("OPEN_API_KEY") )
+result_opeanapi = chat_model_openapi.invoke("Who is President of USA")
+print(result_opeanapi.content)
+
 chat_model_openapi= ChatOpenAI(model = "gpt-4o-2024-11-20",temperature=0, api_key=os.getenv("OPEN_API_KEY") )
-
 result_opeanapi = chat_model_openapi.invoke("Who is President of USA")
 print(result_opeanapi.content)
--- 4_ChatModels/Readme.md
+++ 4_ChatModels/Readme.md
@@ -1,5 +1,5 @@
 Run to get the output of OpenAI_ChatModels 
+
 It seems GPT4 Chat models are trained till Oct 2024, hence it is unable to give latest output
-\n
-![alt text](image.png)
-![alt text](image-1.png)
+
+![alt text](image-2.png)
