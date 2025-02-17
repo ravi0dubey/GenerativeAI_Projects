f142cf5c2778d7f3d8419bee40795d736d6ae0a5
ravi0dubey <ravi0dubey@gmail.com>
2025-02-17 11:04:31 -0500

    adding Gemini ChatModels.py

---
+++ 4_ChatModels/1_Chatmodels_Gemini.py
@@ -0,0 +1,8 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
+print ("Result1 with model gemini-1.5-pro")
+chat_model_gemini = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+result_gemini = chat_model_gemini.invoke("Who is President of USA")
+print(result_gemini.content)
