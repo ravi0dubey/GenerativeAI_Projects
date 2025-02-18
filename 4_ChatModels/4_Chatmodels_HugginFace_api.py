1b45d7f9600dcccfe0b89acf35bcf08df3794de0
ravi0dubey <ravi0dubey@gmail.com>
2025-02-18 07:06:31 -0500

    adding 4_chatmodels_HuggingFace_api.py

---
+++ 4_ChatModels/4_Chatmodels_HugginFace_api.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+print ("Result1 with model HuggingFace")
+llm = HuggingFaceEndpoint(
+    repo_id= "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation"
+)
+
+chat_model_hugginface = ChatHuggingFace(llm=llm)
+result_huggingface = chat_model_hugginface.invoke("Who is President of USA")
+print(result_huggingface.content)
