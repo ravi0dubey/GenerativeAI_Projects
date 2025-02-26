5c602c996f81c335b13d9b1e3f5ac06c09331d17
ravi0dubey <ravi0dubey@gmail.com>
2025-02-26 17:32:46 -0500

    adding

--- 3_LLMs_Models/1_LLMs_OpeniAI.py
+++ 3_LLMs_Models/1_LLMs_OpeniAI.py
@@ -8,4 +8,5 @@ dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
 llm_openai = OpenAI(model = "gpt-3.5-turbo-instruct",api_key=os.getenv("OPEN_API_KEY"))
 result_opeanapi= llm_openai.invoke("Who is Prime Minister of Canada")
 print(result_opeanapi)
+print("hgell")
 
