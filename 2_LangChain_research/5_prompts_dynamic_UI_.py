03e5da9bbf36a9c37bd5c5aaa9a402805d923621
ravi0dubey <ravi0dubey@gmail.com>
2025-03-09 07:49:13 -0400

    Adding 5_prompts_dynamic_UI_.py

---
+++ 2_LangChain_research/5_prompts_dynamic_UI_.py
@@ -0,0 +1,29 @@
+
+from langchain_openai import ChatOpenAI
+import streamlit as st
+import os
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate,load_prompt
+
+model_openapi= ChatOpenAI(model = "gpt-4o-mini",temperature=0, api_key=os.getenv("OPEN_API_KEY") )
+
+st.header('Research Tool_ Dynamic')
+paper_input = st.selectbox("Select Research paper Name",["Attention is All you Need","BERT: Pre-Training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few Shot Learners", "Diffusion Models Beat GANS on Image Synthesis"])
+style_input = st.selectbox("Select Explanation Style",["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"])
+length_input = st.selectbox("Select Explanation Length",["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"])
+
+template = load_prompt("template.json")
+
+
+if st.button('Summarize'):
+    chain = template | model_openapi
+    result= chain.invoke({
+        'paper_input' : paper_input,
+        'style_input': style_input,
+        'length_input':length_input
+    }
+
+    )
+    st.write(result.content)
+
+
--- 2_LangChain_research/Readme.md
+++ 2_LangChain_research/Readme.md
@@ -1,7 +1,10 @@
 # LangChain 
 https://medium.com/@ravi0dubey/langchain-basic-understanding-and-its-components-9a040991bb44
 
-Run command streamlit run .\2_LangChain_research\4_prompts_UI_.py
+# # Running Static Prompts
+
+Run command streamlit run .\2_LangChain_research\4_prompts_static_UI_.py
+
 ![alt text](image-2.png)
 
 We will get URL. Click on the URL. Input screen to enter prompt will be asked
@@ -9,3 +12,15 @@ We will get URL. Click on the URL. Input screen to enter prompt will be asked
 
 Enter the prompt and click on Summarize button, we will get the output as shown below
 ![alt text](image.png)
+
+
+## Running Dymanic Prompt
+
+Run command streamlit run .\2_LangChain_research\5_prompts_dynmaic_UI_.py
+
+![alt text](image-3.png)
+We will get URL. Click on the URL. Input screen to enter prompt will be asked
+![alt text](image-4.png)
+
+Select the type of prompts and and click on Summarize button, we will get the output as shown below
+![alt text](image-5.png)
