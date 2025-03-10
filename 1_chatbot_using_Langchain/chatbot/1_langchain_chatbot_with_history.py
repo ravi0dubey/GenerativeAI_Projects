f65d00f5a58ac7f30b640a935e95f56ded41bdf0
ravi0dubey <ravi0dubey@gmail.com>
2025-03-10 15:26:09 -0400

    1_langchain_chatbot_with_history.py

--- 1_chatbot_using_Langchain/chatbot/1_langchain_chatbot_with_history.py
+++ 1_chatbot_using_Langchain/chatbot/1_langchain_chatbot_with_history.py
@@ -1,22 +1,25 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 import streamlit as st
 import os
 from dotenv import load_dotenv
 
-chat_history = []
+chat_history = [
+    SystemMessage(content='You are a helpful Assistant, give me just 1 line answer')
+]
 
 
 model_openapi= ChatOpenAI(model = "gpt-4o-mini",temperature=0, api_key=os.getenv("OPEN_API_KEY") )
 while True:
     user_input = input('You : ')
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content = user_input))
     if user_input == 'exit':
         break
     result = model_openapi.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content = result.content))
     print(f"AI : {result.content}")
 
 print("Printing complete chat history after exit")
