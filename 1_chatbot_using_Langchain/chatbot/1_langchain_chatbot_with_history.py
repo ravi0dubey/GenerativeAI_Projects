206c4a4dd9722cc2799f137aa1ca1e3badc0a2f6
ravi0dubey <ravi0dubey@gmail.com>
2025-03-10 14:34:56 -0400

    Adding 1_langchain_chatbot_with_history.py

--- 1_chatbot_using_Langchain/chatbot/1_langchain_chatbot_with_history.py
+++ 1_chatbot_using_Langchain/chatbot/1_langchain_chatbot_with_history.py
@@ -6,11 +6,18 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 
+chat_history = []
+
 
 model_openapi= ChatOpenAI(model = "gpt-4o-mini",temperature=0, api_key=os.getenv("OPEN_API_KEY") )
 while True:
     user_input = input('You : ')
+    chat_history.append(user_input)
     if user_input == 'exit':
         break
-    result = model_openapi.invoke(user_input)
+    result = model_openapi.invoke(chat_history)
+    chat_history.append(result.content)
     print(f"AI : {result.content}")
+
+print("Printing complete chat history after exit")
+print(chat_history)
--- 1_chatbot_using_Langchain/chatbot/app.py
+++
@@ -1,9 +0,0 @@
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-
-import streamlit as st
-import os
-from dotenv import load_dotenv
-
-print("hello")
