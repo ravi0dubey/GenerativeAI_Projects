a44c63ac1432ac6519408606c5fd29c660f9857b
ravi0dubey <ravi0dubey@gmail.com>
2025-03-10 13:51:55 -0400

    Adding 5_prompts_dynamic_UI_.py

--- 2_LangChain_research/5_prompts_dynamic_UI_.py
+++ 2_LangChain_research/5_prompts_dynamic_UI_.py
@@ -13,17 +13,14 @@ style_input = st.selectbox("Select Explanation Style",["Beginner-Friendly", "Tec
 length_input = st.selectbox("Select Explanation Length",["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"])
 
 template = load_prompt("template.json")
-
-
-if st.button('Summarize'):
-    chain = template | model_openapi
-    result= chain.invoke({
+prompt = template.invoke({
         'paper_input' : paper_input,
         'style_input': style_input,
         'length_input':length_input
-    }
+    })
 
-    )
+if st.button('Summarize'):
+    result = model_openapi.invoke(prompt)  
     st.write(result.content)
 
 
