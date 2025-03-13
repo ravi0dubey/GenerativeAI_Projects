152534bbd5e13484336a233f772755c8aa93dbab
ravi0dubey <ravi0dubey@gmail.com>
2025-03-13 04:35:06 -0400

    6.1_with_structured_output.py

---
+++ 6_Structured_output/6.1_with_structured_output.py
@@ -0,0 +1,26 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from typing import TypedDict
+import os
+
+load_dotenv()
+
+# Step1 : Declare Model
+model_openapi= ChatOpenAI(model = "gpt-4o-mini",temperature=0, api_key=os.getenv("OPEN_API_KEY") )
+
+# Step2 : Create Typedict Class
+class Review(TypedDict):
+    summary: str
+    sentiment: str
+
+
+
+# Step3 : Invoke Model laden with  TypeDict class
+strcutured_model = model_openapi.with_structured_output(Review)
+result= strcutured_model.invoke("""Paying over 12k and getting a product with scratches how it will feel? The same feeling I had...it had scratches all over the product ....just ban this kind of sellers..and people please buy from a showroom so that you can check and buy...I bought this even after all the negative comments.
+the frame and lens is fine just because the seller had no responsibility of the customers money iam returning this....worst experience I have ever had.
+""")
+
+
+print(f"Summary of Review:{result['summary']}")
+print(f"Sentiment of Review :{result['sentiment']}")
