b93928b112be5c1f89bcdfd899a67fd148352626
ravi0dubey <ravi0dubey@gmail.com>
2025-08-09 14:22:28 -0400

    changes done in prompts_understanding.py

--- 2_LangChain_research/2.1_Prompt_Templates/2.1.1_prompts_understanding.py
+++ 2_LangChain_research/2.1_Prompt_Templates/2.1.1_prompts_understanding.py
@@ -5,16 +5,35 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+load_dotenv(dotenv_path)
+llm_openai = ChatOpenAI(model = "gpt-3.5-turbo",api_key=os.getenv("OPEN_API_KEY"))
+
+
 # 1. Dynamic Prompt
 Dynamic_prompt = PromptTemplate.from_template('Summarize {topic} in {tone} tone')
-print(Dynamic_prompt.format(topic ='Tennis', tone = 'fun'))
-print(Dynamic_prompt.format(topic ='Tennis', tone = 'technical'))
+
+formatted_dynamic_prompt =Dynamic_prompt.format(topic ='Tennis', tone = 'fun')
+result_openapi= llm_openai.invoke(formatted_dynamic_prompt)
+print(result_openapi)
+
+formatted_dynamic_prompt = Dynamic_prompt.format(topic ='Tennis', tone = 'technical')
+result_openapi= llm_openai.invoke(formatted_dynamic_prompt)
+print(result_openapi)
+
+formatted_dynamic_prompt = Dynamic_prompt.format(topic ='Cricket', tone = 'serious')
+result_openapi= llm_openai.invoke(formatted_dynamic_prompt)
+print(result_openapi)
+
 
 # 2. Role-Based Prompt
-role_based_prompt = ChatPromptTemplate.from_template([
-    ("system", "You are a experienced {profession}"),
+role_based_prompt = ChatPromptTemplate.from_messages(
+    [("system", "You are a experienced {profession}"),
     ("user","Tell me about {topic}"),])
-print(role_based_prompt.format_messages(profession="Doctor", topic="Viral Fever"))
+formatted_role_based_prompt= role_based_prompt.format_messages(profession="Doctor", topic="Viral Fever")
+result_openapi= llm_openai.invoke(formatted_role_based_prompt)
+print(result_openapi)
+
 
 # 3. Few shot Prompt
 examples = [
@@ -30,9 +49,14 @@ example_prompt = PromptTemplate(
 example_str = "".join([example_prompt.format(**example) for example in examples])
 
 main_prompt = PromptTemplate(
-    input_variables=["question"]
+    input_variables=["question"],
+    template=f"""
+    Here are some examples:\n{example_str}\nNow answer the following question:\nInput: {{question}}\nOutput:
+    """
+)
 
-
-llm = OpenAI(api_key="your_openai_api_key")
-response = llm(prompt)
-print(response)
+question = "Describe linear regression."
+prompt = main_prompt.format(question=question)
+print(prompt)
+result_openapi= llm_openai.invoke(prompt)
+print(result_openapi)
