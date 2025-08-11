880e9e9b901d34abc7d44df9550330652629e63a
ravi0dubey <ravi0dubey@gmail.com>
2025-08-11 08:35:29 -0400

    Adding changes done to chatmodels_anthropic.py

--- 4_ChatModels/2_Chatmodels_Anthropic.py
+++ 4_ChatModels/2_Chatmodels_Anthropic.py
@@ -1,9 +1,11 @@
 from langchain_anthropic import ChatAnthropic
-import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 print ("Result1 with model claude-3-5-sonnet-20241022")
-model_anthropic = ChatAnthropic(model="claude-3-5-sonnet-20241022", max_tokens=1000, temperature=0,api_key=os.getenv("ANTHROPIC_API_KEY"))
-result_anthropic = model_anthropic.invoke("Who is President of USA")
+model_anthropic = ChatAnthropic(model="claude-3-5-sonnet-20241022", max_tokens=1000, temperature=0)
+result_anthropic = model_anthropic.invoke("who is indian test cricket captain")
 print(result_anthropic)
 
 print("****")
@@ -12,6 +14,12 @@ print(result_anthropic.content)
 
 print("****")
 print ("Result1 with model claude-3-5-haiku-20241022")
-model_anthropic = ChatAnthropic(model="claude-3-5-haiku-20241022", max_tokens=1000, temperature=0,api_key=os.getenv("ANTHROPIC_API_KEY"))
-result_anthropic = model_anthropic.invoke("Who is President of USA")
+model_anthropic = ChatAnthropic(model="claude-3-5-haiku-20241022", max_tokens=1000, temperature=0)
+result_anthropic = model_anthropic.invoke("who is indian test cricket captain")
+print(result_anthropic.content)
+
+print("****")
+print ("Result1 with model claude-opus-4-1-20250805")
+model_anthropic = ChatAnthropic(model="claude-opus-4-1-20250805", max_tokens=1000, temperature=0)
+result_anthropic = model_anthropic.invoke("who is indian test cricket captain")
 print(result_anthropic.content)
