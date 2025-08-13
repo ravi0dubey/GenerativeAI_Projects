b9c89ae4d83dc688e5b5a8563232bad61b297fd3
ravi0dubey <ravi0dubey@gmail.com>
2025-08-12 20:41:00 -0400

    making changes to message_placeholders.py

--- 2_LangChain_research/2.1_Prompt_Templates/2.1.5_chat_prompt_templates.py
+++ 2_LangChain_research/2.1_Prompt_Templates/2.1.5_chat_prompt_templates.py
@@ -1,8 +1,25 @@
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# below code will not work 
+# chat_template = ChatPromptTemplate([
+#   SystemMessage(content='You are a helpful {domain} expert'),
+#   HumanMessage(content= 'Explain in simple terms, what is {topic}')
+# ])
+
+# so we have to give it as tuple instead of above format.
 
 chat_template = ChatPromptTemplate([
     ('system','You are a helpful {domain} expert'),
     ('human','Explain in simple terms, what is {topic}')])
+
+
+model_openapi= ChatOpenAI(model = "gpt-4o-mini",temperature=0, api_key=os.getenv("OPEN_API_KEY") )
 
 prompt = chat_template.invoke({'domain':'football','topic':'offside'})
 print(prompt)
