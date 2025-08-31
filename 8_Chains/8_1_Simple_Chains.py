e84f55a44753a4edd9433532496af935f5bb339b
ravi0dubey <ravi0dubey@gmail.com>
2025-08-31 16:14:38 -0400

    Adding 8_1_Simple_Chains.py

--- 8_Chains/8_1_Simple_Chains.py
+++ 8_Chains/8_1_Simple_Chains.py
@@ -17,15 +17,13 @@ Prompt= PromptTemplate(
 model_openapi= ChatOpenAI(model = "gpt-4",temperature=0,  api_key=os.getenv("OPEN_API_KEY") )
 
 # Step 3 Create Parser
-string_parser = StrOutputParser()
+parser = StrOutputParser()
 
 # Step 4 Create a Chain which does following
-# Template1 which is a prompt1 is passed to the model which generates detailed output. 
-# Detailed output is passed to string output parser  which extracts text from the detailed report.
-# Detailed report is send to template2 to generate 2nd prompt which is sent to model to generate summary report.
-# from summary report , parser will extract the text.
+# Prompt is passed to the model which generates output. 
+# Detailed output is passed to string output parser  which extracts text from the report.
 
-chain = Prompt | model_openapi | string_parser 
+chain = Prompt | model_openapi | parser 
 
 # Step 5 Invoke the chain and pass a topic 
 result = chain.invoke({'topic' : 'Yoga'})
